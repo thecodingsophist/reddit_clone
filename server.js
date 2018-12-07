dc15c88df59cdbb2f4a3d85bee0e7b1d59573a10
Zhanxi Ni <ni.zhanxi@gmail.com>
2018-12-06 19:27:07 -0800

    added nav bar

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'home'}));
+app.set('view engine', 'handlebars');
+
+//ROUTES
+
+app.get('/', (req, res) => {
+    res.render('layouts/home')
+})
+
+//LISTEN
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
